95180b9ef496ec64f319c655d9e6f062d40bdd11
AbdullahAssi <pakabdullah225@gmail.com>
2023-10-11 00:26:03 +0500

    first push

---
+++ test.php
@@ -0,0 +1,179 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./test.css">
+    <title>PakCR</title>
+</head>
+<body>
+    <header class="header">
+        <img src="./logo.jpeg" alt="">
+    </header>
+    <div class="header-bottom">
+        <h1>
+            Orphan Progress Report
+        </h1>
+        <h3>
+            (June2020-July2021)
+        </h3>
+    </div>
+
+    <div class="form">
+        <div class="personal-info">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name">
+            <label for="Father">Father&#160Name:</label>
+            <input type="text" name="Father" id="Father">
+        </div>
+        <div class="personal-info">
+            <label for="Donor">Donor&#160Name:</label>
+            <input type="text" name="Donor" id="Donor">
+            <label for="name">School&#160Name:</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div class="personal-info-3">
+            <label for="Class">Class:</label>
+            <input type="text" name="Class" id="Class">
+            <label for="Age"> Age:</label>
+            <input type="text" name="Age" id="Age">
+            <label for="Cluster">Cluster:</label>
+            <input type="text" name="Cluster" id="Cluster">
+        </div>
+        <div class="interests">
+            <div  class="interests-heading">
+                <h3>Interests & Aspiration</h3>
+            </div>
+        </div>
+        <div class="line"></div>
+        <div class="interest-inputs">
+            <label for="Doctor">Doctor</label>
+            <input type="checkbox" name="interests" id="interests">
+            <label for="Engineer">Engineer</label>
+            <input type="checkbox" name="Engineer" id="Engineer">
+            <label for="Engineer">Teacher</label>
+            <input type="checkbox" name="Teacher" id="Teacher">
+            <label for="Army">Army</label>
+            <input type="checkbox" name="Army" id="Army">
+            <label for="Police">Police</label>
+            <input type="checkbox" name="Police" id="Police">
+            <label for="Civil">Civil&#160Service</label>
+            <input type="checkbox" name="Civil" id="Civil">
+            <label for="Social">Social&#160Worker</label>
+            <input type="checkbox" name="Social" id="Social">
+            <label for="Lawyer">Lawyer/Judge</label>
+            <input type="checkbox" name="lawyer" id="lawyer">
+            <label for="Other">Other:</label>
+            <input type="text" name="Other" id="Other">
+        </div>
+
+        <div class="interests">
+            <div  class="interests-heading">
+                <h3>Education</h3>
+            </div>
+        </div>
+        <div class="line"></div>
+        <h3 class="subtext">Annual</h3>
+        <div class="personal-info marks">
+            <label for="Total">Total&#160Marks:</label>
+            <input type="Number" name="Total" id="Total"  class="number">
+            <label for="Obtained"> Obtained&#160Marks:</label>
+            <input type="Number" name="Obtained" id="Obtained"  class="number">
+            <label for="Cluster">Grade:</label>
+            <input type="text" name="Grade" id="Grade"  class="number">
+            <label for="Percentange">Percentange:</label>
+            <input type="Number" name="Percentange" id="Percentange"  class="number">
+            <label for="Cluster">Remarks:</label>
+            <input type="text" name="Cluster" id="Cluster">
+        </div>
+
+        <div class="interests">
+            <div  class="interests-heading">
+                <h3>Health</h3>
+            </div>
+        </div>
+        <div class="line"></div>
+        <div class="personal-info marks">
+            <label for="Healthy">Healthy:</label>
+            <input type="text" name="Healthy" id="Healthy"  class="number">
+            <label for="Need">Need&#160Attention:</label>
+            <input type="text" name="Need" id="Need"  class="number">
+            <label for="Doctor">Doctor's Comment:</label>
+            <input type="text" name="Doctor" id="Doctor">
+        </div>
+
+        <div class="interests">
+            <div  class="interests-heading">
+                <h3>Activities</h3>
+            </div>
+        </div>
+        <div class="line"></div>
+
+        <div class="box-heading-container">
+            <p>Character Building</p>
+            <p>Food Package</p>
+            <p>Eid Gifts</p>
+            <p>Health</p>
+            <p>Zahiba</p>
+        </div>
+        <div class="box-container">
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+        </div>
+
+        <div class="services-container">
+
+            <div>
+                <h3>-Pakistan Children Relief Provides</h3>
+                <p class="child">-Tution Fee</p>
+                <p class="child">-Books, Notebooks</p>
+                    <p class="child">-Uniform, Notebooks </p>
+                <h3>-Health</h3>
+                <p class="child">-Complete Medical treatment</p>
+                <p class="child">-For referel cases</p>
+            </div>
+
+            <div>
+                <h3>-Dry Food Packages</h3>
+                <h3>-Eid Gifts for all Siblings</h3>
+                <h3>-Zahiba</h3>
+                <h3>-Character Building Activities</h3>
+                <p class="child">-Asmal-ul-husna(Weekly Activities)</p>
+                <p class="child">-National and Religious Days</p>
+            </div>
+ 
+        </div>
+
+        <div class="line-container">
+            <div class="line-footer l1"></div>
+            <div class="line-footer l2"></div>
+        </div>
+        
+        <div class="footer-container">
+            <div class="org">
+                <h3>www.pakcr.org</h3>
+            </div>
+            
+            <div class="social">
+
+                <div>
+                    <p>PakChildren Relief</p>
+                </div>
+                <div>
+                    <p>PakChildren Relief</p>
+                </div>
+            </div>
+
+        </div>
+        
+               
+    </div>
+</body>
+</html>
